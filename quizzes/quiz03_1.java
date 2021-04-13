0b0f86fb47273725141339c65323490b92edc79f
Kagan Ozturkoglu <ozturkoglukagan@gmail.com>
2021-04-13 18:23:24 +0300

    Re-arranged Folders

--- quizzes/quiz03_1.java
+++ quizzes/quiz03_1.java
@@ -1,5 +1,19 @@
 package HW_CS103.quizzes;
 
 public class quiz03_1 {
-    
-}
+
+    static int x = 15; 
+    static int y = 144;
+    public static void main(String[] args) { 
+                calculateSum(x, y);
+        } 
+        public static void calculateSum(int x, int y) { 
+            int sum=0;
+            
+            for(int i=x;i<y+1;i++){
+                sum=sum+i*i;
+            
+            }
+            System.out.println(sum);
+        }
+    }
