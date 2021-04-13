6ff2b0af68e280a3d008c9111bea5484920701ad
Kagan Ozturkoglu <ozturkoglukagan@gmail.com>
2021-04-13 18:23:50 +0300

    Re-arranged Folders

---
+++ Exam_trials/Die.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class Die {
+     
+    public Die(Random random){
+        
+    }
+
+
+
+    public static void main(String[] args) {
+        Random random = new Random();        
+        Die die = new Die(random);
+        System.out.println(die);
+        int outcome = die.roll();
+        System.out.println(die);
+    }
+
+    public int roll() {
+        
+        int outcome = Die(random);
+        
+        
+        return 0;
+        
+    }
+
+}
--- Labs/lab03.java
+++ Labs/lab03.java
@@ -1,10 +1,23 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
 public class lab03{
 
-    
-
-
-
-
-
-
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();             
+        String s = " ";
+        s=scan.nextLine();
+  
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
 }
